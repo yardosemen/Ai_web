a3e8e73b96247ac7a01fd29ab68717b8180344b6
yardosemen <107341054+yardosemen@users.noreply.github.com>
2023-07-13 14:04:30 +0300

    create_project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {},
+		fontFamily: {
+			satoshi: ["Satoshi", "Sans-Serif"],
+			inter: ["inter", "sans-serif"],
+		},
+	},
+	plugins: [],
+};
